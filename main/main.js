997002d20983d9bdef9b79303c080658fe1f4aa7
zxchen8023 <43513652+zxchen8023@users.noreply.github.com>
2018-09-24 19:28:51 +0800

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(array) {
   // Write your code here
+  var len = array.length;
+  var sum = Number(array[0]);
+  for(var i = 1; i < len; i++){
+	  sum = sum + Number(array[i]);
+  }
+  return sum;
 };
